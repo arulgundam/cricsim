30adc2f4ea393344c2c32d5740b1f5008291a81b
arulgundam <arul.gundam@gmail.com>
2021-04-03 11:04:49 -0400

    feat(home): finished outline

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,22 @@
 <template>
-  <div class="home">
-    heyyy
-  </div>
+  <main class="home">
+    <header class="toolbar">
+      <div>
+        CricSim
+      </div>
+      <div>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/squad">Squad Data</router-link>
+      </div>
+    </header>
+    <div class="body">
+      <img id="stadiumimg" src="/images/tare.jpg">
+      Simulate T20 games by choosing your favorite players!
+      <router-link to="/settings" custom v-slot="{navigate}">
+        <button @click="navigate">Start Game</button>
+      </router-link>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -11,3 +26,33 @@ export default {
   name: 'Home'
 }
 </script>
+<style>
+.home {
+  width: 100%;
+  height: 100%;
+}
+
+.toolbar {
+  box-sizing: border-box;
+  width: 100%;
+  height: 100px;
+  padding: 20px 0 20px 0; /* top right bottom left */
+  text-align: center;
+  line-height: 60px;
+  font-size: 28px;
+  display: flex;
+  justify-content: space-between;
+}
+
+#stadiumimg {
+  width: 66%;
+}
+
+.body {
+  height: calc(100% - 100px);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
